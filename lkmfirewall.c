6b38ee1bdbcad80b34b2501184efff6bf17ef269
its20pastday <gjlee@ucsb.edu>
2019-01-20 14:26:03 -0800

    Fixed parameters for nf_hook_fn

--- lkmfirewall.c
+++ lkmfirewall.c
@@ -1,12 +1,34 @@
 #include <linux/module.h> //For all modules
 #include <linux/kernel.h> //For KERN_INFO
+#include <linux/netfilter.h>
+#include <linux/netfilter_ipv4.h>
+#define AUTHOR "Garrett Lee <gjlee@ucsb.edu>"
+#define DESC "Basic firewall using netfilter framework"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR(AUTHOR);
+MODULE_DESCRIPTION(DESC);
+
+static struct nf_hook_ops hk;
+
+unsigned int nf_hook_ex(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
+	return NF_DROP;
+}
 
 int init_module(void) {
-	printk(KERN_INFO "Hello world 1.\n");
+	hk = (struct nf_hook_ops) {
+		.hook = nf_hook_ex,
+		.hooknum = NF_INET_PRE_ROUTING,
+		.pf = PF_INET,
+		.priority = NF_IP_PRI_FIRST
+	};
+	nf_register_hook(&hk);
+
+
 	return 0; // return status, tells if module is loaded
 }
 
 void cleanup_module(void) {
-	printk(KERN_INFO "Goodbye world 1.\n");
+	nf_unregister_hook(&hk); //disconnect our func handler
 }
 
