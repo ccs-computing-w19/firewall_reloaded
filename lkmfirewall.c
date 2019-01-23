fcce1cc7f60244f04eea7a269c531b5d5ff16f62
its20pastday <gjlee@ucsb.edu>
2019-01-22 17:15:29 -0800

    Able to store commandline args using keyword args

--- lkmfirewall.c
+++ lkmfirewall.c
@@ -3,6 +3,9 @@
 #include <linux/netfilter.h>
 #include <linux/netfilter_ipv4.h>
 #include <linux/ip.h>
+#include <linux/moduleparam.h> //Allows cmdl args
+#include <linux/init.h>
+#include <linux/stat.h>
 #define AUTHOR "Garrett Lee <gjlee@ucsb.edu>"
 #define DESC "Basic firewall using netfilter framework"
 
@@ -12,12 +15,22 @@ MODULE_DESCRIPTION(DESC);
 
 static struct nf_hook_ops hk;
 
+static char *option = "a"; 
+static char *ip = "127.0.0.1";
+
+module_param(option, charp, 0);
+MODULE_PARM_DESC(option, "Designates whether an ip or port is to be blocked");
+module_param(ip, charp, 0);
+MODULE_PARM_DESC(ip, "IP to be blocked");
+
 unsigned int nf_hook_ex(void *priv, struct sk_buff *skb, const struct nf_hook_state *state) {
 	printk(KERN_INFO "Packet dropped\n");
 	return NF_DROP;
 }
 
 int init_module(void) {
+	printk(KERN_INFO "Option received is %s\n", option);
+	printk(KERN_INFO "IP received is %s\n", ip);
 	hk = (struct nf_hook_ops) {
 		.hook = nf_hook_ex,
 		.hooknum = NF_INET_PRE_ROUTING,
