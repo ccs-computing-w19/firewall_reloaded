3fe7a415a4e99813e3192ac97215c75cec989c02
its20pastday <gjlee@ucsb.edu>
2019-02-01 11:15:26 -0800

    Adds option to specify port from commandline, removes option

--- lkmfirewall.c
+++ lkmfirewall.c
@@ -16,14 +16,13 @@ MODULE_DESCRIPTION(DESC);
 
 static struct nf_hook_ops hk;
 
-static char *option = "a"; 
-static char *ip = "8.8.8.8";
-static int port = 5001;
+static char *ip = 0; //Defaults to None
+static int port = 0; //Defaults to None
 
-module_param(option, charp, 0);
-MODULE_PARM_DESC(option, "Designates whether an ip or port is to be blocked");
 module_param(ip, charp, 0);
 MODULE_PARM_DESC(ip, "IP to be blocked");
+module_param(port, int, 0);
+MODULE_PARM_DESC(int, "Port to be blocked");
 
 unsigned int inet_addr(char *ip) { 
 
@@ -49,18 +48,24 @@ unsigned int nf_hook_fn(void *priv, struct sk_buff *skb, const struct nf_hook_st
 	printk(KERN_INFO "Source MAC: %pM, Dest. MAC %pM\n", eth->h_source, eth->h_dest);
 	printk(KERN_INFO "Source IP: %pI4\n", &ip_head->saddr);
 
-	if (ip_head->saddr==inet_addr(ip)){ //Checks for matching IP address
-		printk(KERN_INFO "Rejecting packet with IP: %pI4\n",&ip_head->saddr);
-		return NF_DROP; //Drops the packet
+	if (ip) {
+
+		if (ip_head->saddr==inet_addr(ip)){ //Checks for matching IP address
+			printk(KERN_INFO "Rejecting packet with IP: %pI4\n",&ip_head->saddr);
+			return NF_DROP; //Drops the packet
+		}
+
+		if (ip_head->protocol == IPPROTO_TCP) { //Checks if protocol is TCP
+			tcp = (struct tcphdr*)skb_transport_header(skb); //Puts data into a tcp header so we can look at L4 info
+		}
 	}
 
-	if (ip_head->protocol == IPPROTO_TCP) { //Checks if protocol is TCP
-		tcp = (struct tcphdr*)skb_transport_header(skb); //Puts data into a tcp header so we can look at L4 info
-	}
+	if (port) {
 
-	if (tcp && tcp->dest==port) {
-		printk(KERN_INFO "Rejecting packet to port: %d", port);
-		return NF_DROP;
+		if (tcp && tcp->dest==port) {
+			printk(KERN_INFO "Rejecting packet to port: %d", port);
+			return NF_DROP;
+		}
 	}
 
 	return NF_ACCEPT;
@@ -68,9 +73,8 @@ unsigned int nf_hook_fn(void *priv, struct sk_buff *skb, const struct nf_hook_st
 
 int init_module(void) {
 
-	printk(KERN_INFO "Option received is %s\n", option);
 	printk(KERN_INFO "IP received is %s\n", ip);
-	printk(KERN_INFO "Returns %x", inet_addr(ip));
+	printk(KERN_INFO "Port received is %d\n", port);
 	 
 	hk = (struct nf_hook_ops) {
 		.hook = nf_hook_fn,
