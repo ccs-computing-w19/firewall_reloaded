02f89d84be58861eafe03ba9268159073371d276
its20pastday <gjlee@ucsb.edu>
2019-02-17 13:41:26 -0800

    Implements linked list data structure to hold rules

--- Makefile
+++ Makefile
@@ -2,12 +2,12 @@ obj-m += lkmfirewall.o
 
 all:
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
-clean:
+clean: fwhelper
 	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
 	rm fwhelper
 test:
 	sudo insmod lkmfirewall.ko
 	sudo rmmod lkmfirewall
 	dmesg
-helper:
+helper: fwhelper.cpp
 	g++ -g fwhelper.cpp -o fwhelper
--- lkmfirewall.c
+++ lkmfirewall.c
@@ -7,12 +7,24 @@
 #include <linux/moduleparam.h> //Allows cmdl args
 #include <linux/init.h>
 #include <linux/stat.h>
+#include <linux/list.h>
 #define AUTHOR "Garrett Lee <gjlee@ucsb.edu>"
 #define DESC "Basic firewall using netfilter framework"
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR(AUTHOR);
 MODULE_DESCRIPTION(DESC);
+
+struct rule {
+	unsigned int src_ip;
+	unsigned int dest_ip;
+	unsigned int src_port;
+	unsigned int dest_port;
+	struct list_head list;
+};
+
+struct list_head rule_list;
+LIST_HEAD(rule_list); //Sets next and prev to itself
 
 static struct nf_hook_ops hk;
 
@@ -48,6 +60,32 @@ unsigned int nf_hook_fn(void *priv, struct sk_buff *skb, const struct nf_hook_st
 	printk(KERN_INFO "Source MAC: %pM, Dest. MAC %pM\n", eth->h_source, eth->h_dest);
 	printk(KERN_INFO "Source IP: %pI4\n", &ip_head->saddr);
 
+	struct list_head *pos = NULL; //Determines which Node we are pointing at when we iterate
+	struct rule *dataptr = NULL;
+	list_for_each(pos, &rule_list) { //Iterates through all rules of the linked list
+		dataptr = list_entry(pos, struct rule, list); //Uses offset to obtain addr of our whole struct from the list_head address
+		if (dataptr->src_ip) {
+
+			if (ip_head->saddr==dataptr->src_ip){ //Checks for matching IP address
+				printk(KERN_INFO "Rejecting packet with IP: %pI4\n",&ip_head->saddr);
+				return NF_DROP; //Drops the packet
+			}
+
+			if (ip_head->protocol == IPPROTO_TCP) { //Checks if protocol is TCP
+				tcp = (struct tcphdr*)skb_transport_header(skb); //Puts data into a tcp header so we can look at L4 info
+			}
+		}
+
+		if (dataptr->dest_port) {
+
+			if (tcp && tcp->dest==dataptr->dest_port) {
+				printk(KERN_INFO "Rejecting packet to port: %d", port);
+				return NF_DROP;
+			}
+		}
+	}
+
+	/*
 	if (ip) {
 
 		if (ip_head->saddr==inet_addr(ip)){ //Checks for matching IP address
@@ -67,6 +105,7 @@ unsigned int nf_hook_fn(void *priv, struct sk_buff *skb, const struct nf_hook_st
 			return NF_DROP;
 		}
 	}
+	*/
 
 	return NF_ACCEPT;
 }
@@ -75,7 +114,18 @@ int init_module(void) {
 
 	printk(KERN_INFO "IP received is %s\n", ip);
 	printk(KERN_INFO "Port received is %d\n", port);
-	 
+
+	struct rule *options;
+       	options =  kmalloc(sizeof(*options), GFP_KERNEL);
+	if (options == NULL) {
+		printk(KERN_INFO "Error allocating memory");
+		return -1;
+	}
+	options->src_ip = inet_addr(ip);
+	options->dest_port = port;
+	INIT_LIST_HEAD(&(options->list));
+	list_add(&(options->list), &rule_list);
+
 	hk = (struct nf_hook_ops) {
 		.hook = nf_hook_fn,
 		.hooknum = NF_INET_PRE_ROUTING,
